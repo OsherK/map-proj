d03da9f889d6cf41466bb56b89b017b17de3932f
OsherK <osherzasx@gmail.com>
2020-08-05 17:46:33 +0300

    checking maps

--- main/util-service.js
+++ main/util-service.js
@@ -7,3 +7,8 @@ function onInit() {
 function getRandomInteger(max, min = 0) {
     return Math.floor(Math.random() * (max - min) + min + 1);
 }
+
+map = new google.maps.Map(document.querySelector('#map'), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8
+});
